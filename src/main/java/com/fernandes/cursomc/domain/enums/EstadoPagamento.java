34cc2d43568687cd59a61dafed13d8e00d2c896e
Fernandes <breynerfernandes@gmail.com>
2021-04-07 01:02:34 -0300

    Pedido, EstadoPagamento e Pagamamento - 1

--- src/main/java/com/fernandes/cursomc/domain/enums/EstadoPagamento.java
+++ src/main/java/com/fernandes/cursomc/domain/enums/EstadoPagamento.java
@@ -1,13 +1,13 @@
 package com.fernandes.cursomc.domain.enums;
 
-public enum TipoCliente {
+public enum EstadoPagamento {
 
-	PESSOAFISICA(1, "Pessoa física"), PESSOAJURIDICA(2, "Pessoa Jurídica");
+	PENDENTE(1, "Pendente"), QUITADO(2, "Quitado"), CANCELADO(3, "Cancelado");
 
 	private int cod;
 	private String descricao;
 
-	private TipoCliente(int cod, String descricao) {
+	private EstadoPagamento(int cod, String descricao) {
 		this.cod = cod;
 		this.descricao = descricao;
 
@@ -23,12 +23,12 @@ public enum TipoCliente {
 
 	}
 
-	public static TipoCliente toEnum(Integer cod) {
+	public static EstadoPagamento toEnum(Integer cod) {
 		if (cod == null) {
 			return null;
 		}
 
-		for (TipoCliente x : TipoCliente.values()) {
+		for (EstadoPagamento x : EstadoPagamento.values()) {
 			if (cod.equals(x.getCod())) {
 				return x;
 			}
@@ -36,4 +36,5 @@ public enum TipoCliente {
 
 		throw new IllegalArgumentException("Id Inválido: " + cod);
 	}
+
 }

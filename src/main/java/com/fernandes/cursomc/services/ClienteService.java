74de10adf027e619c3774d3cf5f8156092b5e31d
Fernandes <breynerfernandes@gmail.com>
2021-04-20 17:59:51 -0300

    PUT, DELETE e GET para Clientes (1)

--- src/main/java/com/fernandes/cursomc/services/ClienteService.java
+++ src/main/java/com/fernandes/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.fernandes.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.fernandes.cursomc.domain.Cliente;
+import com.fernandes.cursomc.dto.ClienteDTO;
 import com.fernandes.cursomc.repositories.ClienteRepository;
+import com.fernandes.cursomc.services.exceptions.DataIntegrityException;
 import com.fernandes.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -21,4 +28,31 @@ public class ClienteService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
 
+	public Cliente update(Cliente obj) {
+		find(obj.getId());
+		return repo.save(obj);
+
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO objDto) {
+		throw new UnsupportedOperationException();
+	}
 }

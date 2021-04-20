90632f62d9fafe0957f9c484783a346db8d96ce9
Fernandes <breynerfernandes@gmail.com>
2021-04-20 15:22:55 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/fernandes/cursomc/services/CategoriaService.java
+++ src/main/java/com/fernandes/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.fernandes.cursomc.domain.Categoria;
+import com.fernandes.cursomc.dto.CategoriaDTO;
 import com.fernandes.cursomc.repositories.CategoriaRepository;
 import com.fernandes.cursomc.services.exceptions.DataIntegrityException;
 import com.fernandes.cursomc.services.exceptions.ObjectNotFoundException;
@@ -52,7 +53,11 @@ public class CategoriaService {
 	}
 
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage,Direction.valueOf(direction), orderBy);
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }

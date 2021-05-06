41e6bac867b3ef2d85dd4135dfbeae1bf1753440
Fernandes <breynerfernandes@gmail.com>
2021-05-06 12:06:20 -0300

    Validação customizada: e-mail não repetido na inserção de cliente

--- src/main/java/com/fernandes/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/fernandes/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,29 +6,43 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.fernandes.cursomc.domain.Cliente;
 import com.fernandes.cursomc.domain.enums.TipoCliente;
 import com.fernandes.cursomc.dto.ClienteNewDTO;
+import com.fernandes.cursomc.repositories.ClienteRepository;
 import com.fernandes.cursomc.resources.exception.FieldMessage;
 import com.fernandes.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Autowired
+	private ClienteRepository repo;
+
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
 
 	@Override
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
-		
+
 		List<FieldMessage> list = new ArrayList<>();
-		
+
 		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage ("cpfOuCnpj", "CPF inválido"));
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
 		}
-		
+
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage ("cpfOuCnpj", "CNPJ inválido"));
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
-		
+
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "E-mail já existente."));
+
+		}
+
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())

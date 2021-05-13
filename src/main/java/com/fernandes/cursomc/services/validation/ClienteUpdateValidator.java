58955db32212879acbd25fbc885b957f2d4c2f17
Fernandes <breynerfernandes@gmail.com>
2021-05-13 14:45:22 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/fernandes/cursomc/services/validation/ClienteUpdateValidator.java
+++ src/main/java/com/fernandes/cursomc/services/validation/ClienteUpdateValidator.java
@@ -34,7 +34,6 @@ public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate
 		
 		@SuppressWarnings("unchecked")
 		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
-		@SuppressWarnings("unused")
 		Integer uriId = Integer.parseInt(map.get("id"));
 		
 		List<FieldMessage> list = new ArrayList<>();
